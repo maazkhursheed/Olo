36bb77eb2ce201c14bce4d6be1c324340f175990
yahyaw786 <yahyawasim@live.com>
2016-06-02 17:23:20 +0500

    listview alternate color issue fixed

--- app/src/main/java/adapters/MenuAdapter.java
+++ app/src/main/java/adapters/MenuAdapter.java
@@ -1,5 +1,6 @@
 package adapters;
 
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,6 +20,9 @@ public class MenuAdapter extends RecyclerView.Adapter<MenuAdapter.MyViewHolder> 
 
     private List<MenusItem> menusList;
     OnItemClickListner menuItemClickListner;
+    ////
+//    private MyViewHolder viewHolder ;
+//    private View layoutView;
 
     public MenuAdapter(List<MenusItem> menusList) {
         this.menusList = menusList;
@@ -31,9 +35,7 @@ public class MenuAdapter extends RecyclerView.Adapter<MenuAdapter.MyViewHolder> 
 
         public MyViewHolder(View view) {
             super(view);
-//            title = (TextView) view.findViewById(R.id.title);
-//            genre = (TextView) view.findViewById(R.id.genre);
-//            year = (TextView) view.findViewById(R.id.year);
+
             menu_item_name = (TextView) view.findViewById(R.id.item_name);
             menus_item_desc = (TextView) view.findViewById(R.id.item_description);
             menu_item_price = (TextView) view.findViewById(R.id.item_price);
@@ -60,29 +62,56 @@ public class MenuAdapter extends RecyclerView.Adapter<MenuAdapter.MyViewHolder> 
         this.menuItemClickListner = menuItemClickListner;
     }
 
-//    @Override
-//    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-//        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_menu_item, parent, false);
-//
-//        return new MyViewHolder(itemView);
-//    }
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.custom_menu_item_row, parent, false);
-
+         //itemView.setBackgroundColor(R.color.cardview_shadow_end_color);
         return new MyViewHolder(itemView);
+////
+//        layoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout.custom_menu_item_row, parent, false);
+//
+//
+//        viewHolder = new MyViewHolder(layoutView);
+//        layoutView.setTag(viewHolder);
+//
+//
+//        viewHolder = (MyViewHolder) layoutView.getTag();
+//
+//        return viewHolder;
+
     }
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
         MenusItem menus = menusList.get(position);
-        holder.menu_item_name.setText(""+menus.getName());
-        holder.menus_item_desc.setText(""+menus.getDescription());
-        holder.menu_item_price.setText("Rs"+":"+(int) menus.getPrice());
+        if (menus.getDescription().isEmpty())
+        {
+            holder.menus_item_desc.setText("Very Delicious in taste");
 
-//        holder.title.setText(menus.getTitle());
-//        holder.genre.setText(menus.getGenre());
-//        holder.year.setText(menus.getYear());
+        }
+        else {
+            holder.menu_item_name.setText("" + menus.getName());
+
+            holder.menus_item_desc.setText("" + menus.getDescription());
+            holder.menu_item_price.setText("Rs" + ":" + (int) menus.getPrice());
+        }
+      //  holder.setIsRecyclable(true);
+    //    holder.itemView.setBackgroundColor(Color.parseColor("#ffffcc"));
+       // layoutView.setBackgroundResource(R.drawable.row_item_bg);
+//        if(position % 2 == 0){
+//
+//           // layoutView.setBackgroundResource(R.drawable.row_item_bg);
+//           // layoutView.setBackgroundColor(Color.GRAY);
+//            holder.itemView.setBackgroundColor(Color.parseColor("#dddddd"));
+//        }
+
+        if (position % 2 == 1) {
+            holder.itemView.setBackgroundColor(Color.parseColor("#dddddd"));
+        } else {
+            holder.itemView.setBackgroundColor(Color.parseColor("#ffffff"));
+        }
+
+
     }
 
     @Override
