8c7a23469c101ea4a5552525d8088e0ac9af26fb
yahyaw786 <yahyawasim@live.com>
2016-06-03 20:20:13 +0500

    Refractor the code of Details screen

--- app/src/main/java/com/example/maaz/olo/screens/DetailScreen.java
+++ app/src/main/java/com/example/maaz/olo/screens/DetailScreen.java
@@ -1,5 +1,6 @@
 package com.example.maaz.olo.screens;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -14,99 +15,112 @@ import utils.IntentsConstants;
 /**This is detail screen of meal items
  *
  */
-public class DetailScreen extends AppCompatActivity implements View.OnClickListener {
+public class DetailScreen extends AppCompatActivity {
 
     private Toolbar toolbar;
-    private TextView item_name,item_price,add_quantity,subtract_quantity, quantity_totalprice,item_quantity;
-    private Button add_cart_btn;
-    private double itemprice;
+    private TextView label_itemName,
+                     label_itemPrice,
+                     label_incrementQuantity,
+                     label_decrementQuantity,
+                     label_totalPrice,
+                     label_quantity;
+
+    private Button button_addCart;
+    private double itemPrice;
     static  double totalprice;
     static double total_cart_bill;
     private boolean valuechanged=false;
     private int minQuantity=1,maxQuantity=20;
     private int selectedItemQuantity =1;
+    private String itemname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-      setContentView(R.layout.detailed_item_screen_withpicker);
+        setContentView(R.layout.detailed_item_screen_withpicker);
 
-
+        getIntentValues();
         initViews();
-        setHomeButton();
-        get_set_intentdata();
-        setInitial_itemPrice();
-
-        send_data_tomainmenu();
 
 
 
     }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return true;
+    }
+
+
+//=======================================Helper Methods=======================================================
     public void setHomeButton()
     {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
     }
-    private void initViews()
-    {
+
+
+    private void initViews() {
         toolbar = (Toolbar) findViewById(R.id.toolbar_details_item_screen);
-    //   quantity_picker= (NumberPicker) findViewById(R.id.quantity_numberPicker);
-        add_cart_btn= (Button) findViewById(R.id.addcart_btn);
-        item_name= (TextView) findViewById(R.id.textView_itemname);
-        item_price= (TextView) findViewById(R.id.textView_itemPrice);
-        add_quantity= (TextView) findViewById(R.id.add_txtview);
-       subtract_quantity= (TextView) findViewById(R.id.subtract_txtview);
-        quantity_totalprice = (TextView) findViewById(R.id.total_price);
-        item_quantity= (TextView) findViewById(R.id.itemquantity_txtview);
+
+        button_addCart = (Button) findViewById(R.id.addcart_btn);
+        label_itemName = (TextView) findViewById(R.id.textView_itemname);
+        label_itemPrice = (TextView) findViewById(R.id.textView_itemPrice);
+        label_incrementQuantity = (TextView) findViewById(R.id.add_txtview);
+        label_decrementQuantity = (TextView) findViewById(R.id.subtract_txtview);
+        label_totalPrice = (TextView) findViewById(R.id.total_price);
+        label_quantity = (TextView) findViewById(R.id.itemquantity_txtview);
         selectedItemQuantity=minQuantity;
-        item_quantity.setText("Quantity :"+selectedItemQuantity);
-        add_quantity.setOnClickListener(this);
-        subtract_quantity.setOnClickListener(this);
-//        quantity_picker.setMinValue(1);
-//        quantity_picker.setMaxValue(10);
-//        quantity_picker.setWrapSelectorWheel(false);
+        label_quantity.setText("Quantity: "+selectedItemQuantity);
 
+
+        label_itemName.setText(itemname);
+        label_itemPrice.setText("Rs: "+ String.valueOf(itemPrice));
+        totalprice= itemPrice;
+
+
+        setHomeButton();
+
+        setInitial_itemPrice();
+
+        setListeners();
+    }
+
+    private void setListeners() {
+        button_addCart.setOnClickListener(new AddCartListener());
+        label_incrementQuantity.setOnClickListener(new IncrementListener());
+        label_decrementQuantity.setOnClickListener(new DecrementListener());
+    }
+
+
+
+    /**This method receives intent values from previous screen and set them in
+     * appropriate instance variables
+     *
+     */
+    private void getIntentValues() {
+
+        itemname=  getIntent().getExtras().getString(IntentsConstants.item_name);
+        itemPrice = getIntent().getExtras().getDouble(IntentsConstants.item_price);
 
     }
-    private void send_data_tomainmenu()
-    {
-        add_cart_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//
-                    total_cart_bill +=totalprice;
-                   // Intent intent = new Intent(getApplicationContext(), MainActivity.class);
 
-                   // startActivity(intent);
-                    finish();
-
-//
-//
-           }
-        });
-
-    }
-    private void get_set_intentdata()
-    {
-        String itemname_i;
-        double itemprice_i;
-
-        itemname_i=getIntent().getExtras().getString(IntentsConstants.item_name);
-        itemprice_i=getIntent().getExtras().getDouble(IntentsConstants.item_price);
-        item_name.setText(String.valueOf(itemname_i).toString());
-        item_price.setText("Rs :"+ String.valueOf(itemprice_i));
-        itemprice=itemprice_i;
-
-
-    }
     //set initial total price total price in txtview when activity created
     private void setInitial_itemPrice()
     {
-        if(quantity_totalprice.getText().equals(""))
+
+        calcTotalPrice(selectedItemQuantity, itemPrice);
+        if(label_totalPrice.getText().equals(""))
         {
-            quantity_totalprice.setText("" + itemprice);
+            label_totalPrice.setText("" + itemPrice);
         }
 
     }
@@ -121,56 +135,52 @@ public class DetailScreen extends AppCompatActivity implements View.OnClickListe
         return quantity*itemprice;
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
+
+
+
+
+
+
+//==========================================Inner Classes / Listeners=========================================
+    private class AddCartListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            total_cart_bill +=totalprice;
+
+//            Intent intent = new Intent(DetailScreen.this,MainActivity.class);
+//            startActivity(intent);
+
+            finish();
+        }
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                finish();
-                //Toast.makeText(getApplicationContext(),"Back button clicked", Toast.LENGTH_SHORT).show();
-                break;
+    private class IncrementListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            if(selectedItemQuantity==minQuantity||selectedItemQuantity <maxQuantity)
+            {
+                selectedItemQuantity++;
+                label_quantity.setText("Quantity :"+selectedItemQuantity);
+                totalprice= calcTotalPrice(selectedItemQuantity, itemPrice);
+                label_totalPrice.setText(""+totalprice);
+
+            }
         }
-        return true;
     }
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()){
-//     increemnent quantity here
-            case R.id.add_txtview:
-                if(selectedItemQuantity==minQuantity||selectedItemQuantity <maxQuantity)
-                {
-                    selectedItemQuantity++;
-                    item_quantity.setText("Quantity :"+selectedItemQuantity);
-                   totalprice= calcTotalPrice(selectedItemQuantity,itemprice);
-                    quantity_totalprice.setText(""+totalprice);
+    private class DecrementListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            if(selectedItemQuantity>minQuantity)
+            {
 
-                }
-                break;
-//     decreemnent quantity  here
-            case R.id.subtract_txtview:
+                selectedItemQuantity--;
+                //selectedItemQuantity=minQuantity;
+                label_quantity.setText("Quantity :"+selectedItemQuantity);
+                totalprice= calcTotalPrice(selectedItemQuantity, itemPrice);
+                label_totalPrice.setText(""+totalprice);
 
-
-                if(selectedItemQuantity>minQuantity)
-                {
-
-                    selectedItemQuantity--;
-                    //selectedItemQuantity=minQuantity;
-                    item_quantity.setText("Quantity :"+selectedItemQuantity);
-                    totalprice= calcTotalPrice(selectedItemQuantity,itemprice);
-                    quantity_totalprice.setText(""+totalprice);
-
-                }
-                break;
-
-
+            }
         }
-
-
-
     }
 }
